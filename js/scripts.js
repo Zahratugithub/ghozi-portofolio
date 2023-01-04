1e33ab4ac9084983655835ef071c8e70fcbce8c2
GhoSYwaridi <ghoziwaridi@gmail.com>
2023-01-04 08:51:56 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// const header = document.querySelector(".header");
+// const tombol = document.querySelector(".nav-tonggel");
+
+// tombol.addEventListener("click", () => {
+//     const visibility = header.getAttribute("data-visibility");
+
+//     if (visibility === "false") {
+//         header.setAttribute("data-visibility", true);
+//     } else if (visibility === "true") {
+//         header.setAttribute("data-visibility", false);
+//     }
+// });
